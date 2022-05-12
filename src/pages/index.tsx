7b8d670b940e210af8f40cf375cb02c35cce0ff6
Peu Silva <silvasantospedro91@gmail.com>
2022-05-12 07:53:40 -0300

    husky and lint-staged

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 
 type Props = {
-  title: string,
+  title: string
 }
 
-export default function Home({title = "React Avançado"}: Props) {
+export default function Home({ title = 'React Avançado' }: Props) {
+  const bla = 'var'
 
   return (
     <div className={styles.container}>
