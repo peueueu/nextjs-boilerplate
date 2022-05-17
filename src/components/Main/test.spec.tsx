5b017a54d723038f09162445f4ba050c4bf82f24
Peu Silva <silvasantospedro91@gmail.com>
2022-05-16 22:32:53 -0300

    add a test to main component

--- src/components/Main/test.spec.tsx
+++ src/components/Main/test.spec.tsx
@@ -3,7 +3,7 @@ import { screen, render } from '@testing-library/react'
 import Main from '.'
 
 describe('<Main />', () => {
-  it('should render the heading', () => {
+  it('should render the heading with the default values if no props are passed', () => {
     const { container } = render(<Main />)
 
     expect(
@@ -11,6 +11,17 @@ describe('<Main />', () => {
     ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the heading with the correct values', () => {
+    const props = { title: 'Teste', description: 'Just a simple text' }
+
+    render(<Main {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+    expect(screen.getByText(props.description)).toBeInTheDocument()
   })
 
   it('should render the colors correctly', () => {
